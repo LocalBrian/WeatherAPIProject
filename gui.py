b3080b42015175209d17265d3017eb62d6a17cd8
LocalBrian <150453691+LocalBrian@users.noreply.github.com>
2025-05-21 15:52:26 -0500

    Update gui.py
    getting some base code in.

--- gui.py
+++ gui.py
@@ -1,4 +1,23 @@
 # Will handle all of the GUI related code
 # Importing the necessary libraries
-import tkinter as tk
+import tkinter as tkin
 
+class WeatherGUI:
+  def __init__(self, title="No Title Given", width=300, height = 200):
+    # Create root window
+    self.root = tkin.Tk()
+    # root window title and dimension
+    self.root.title(title)
+    self.root.geometry(f"{wdith}x{height}")
+
+  def start_up_welcome(self):
+    
+
+def test_gui():
+  # Create  root window
+  test_gui = WeatherGUI("Testing GUI",400,300)
+
+  # 
+
+if __name__ == "__main__":
+  test_gui()
